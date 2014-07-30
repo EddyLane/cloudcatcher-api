feab57cfa56e95686c359ffb5bd217fb28a8bb2e
Eddy Lane <eddy@uvd.co.uk>
2014-07-30 18:11:36 +0100

    started using the google feed API instead of just the XmlReader

--- src/Fridge/FirebaseBundle/Consumer/ReadNode.php
+++ src/Fridge/FirebaseBundle/Consumer/ReadNode.php
@@ -9,6 +9,7 @@
 namespace Fridge\FirebaseBundle\Consumer;
 
 use Fridge\FirebaseBundle\Client\FirebaseClient;
+use GuzzleHttp\Client;
 use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
 use PhpAmqpLib\Message\AMQPMessage;
 use Symfony\Component\HttpKernel\Log\LoggerInterface;
@@ -16,71 +17,73 @@ use Symfony\Component\HttpKernel\Log\LoggerInterface;
 class ReadNode implements ConsumerInterface
 {
     private $logger;
-
+    private $xmlReader;
     private $client;
 
     public function __construct(FirebaseClient $client, LoggerInterface $logger)
     {
         $this->logger = $logger;
         $this->client = $client;
+        $this->xmlReader = new \XMLReader();
     }
 
     public function execute(AMQPMessage $msg)
     {
         $data = $this->client->getClient()->get('/users/' . $msg->body . '/podcasts');
         $this->logger->info(sprintf('Node processing:', $msg->body));
-        $xmlReader = new \XMLReader();
+        $guzzleClient = new Client();
 
         foreach ($data as $i => $podcast) {
 
             $new = 0;
             $heard = 0;
-            $episodeIndex = 0;
-            $latest = null;
 
-            if (!$xmlReader->open($podcast['feed'])) {
-                $this->logger->error(sprintf('Could not get feed "%s"', $podcast['feed']));
-                continue;
-            }
+            $responseJson = $guzzleClient->get(
+                'https://ajax.googleapis.com/ajax/services/feed/load',
+                [
+                    'query' => [
+                        'q' => $podcast['feed'],
+                        'v' => '1.0',
+                        'output' => 'json',
+                        'num' => -1
+                    ]
+                ]
+            );
+
+            $episodes = $responseJson->json()['responseData']['feed']['entries'];
 
 
-            while ($xmlReader->read() && $episodeIndex < 100) {
+            foreach($episodes as $episode) {
 
-                if ($xmlReader->localName === 'pubDate' && is_null($latest)) {
-                    $xmlReader->read();
-                    $latest = $xmlReader->value;
+                $this->logger->warn(print_r(array_keys($episode)));
+
+                if (isset($podcast['heard']) && in_array($episode['link'], $podcast['heard'])) {
+                    $heard++;
+                } else {
+                    $new++;
                 }
 
-                if ($xmlReader->getAttribute('url')) {
-
-                    $episodeIndex++;
-
-                    if (isset($podcast['heard']) && in_array($xmlReader->getAttribute('url'), $podcast['heard'])) {
-                        $heard++;
-                    } else {
-                        $new++;
-                    }
-                }
             }
+
+            $date = new \DateTime($episodes[0]['publishedDate']);
+            $latest = $date->format(\DateTime::ISO8601);
 
             $this->logger->info(
                 sprintf(
-                    'For user "%s" we found %d new and %d heard episodes for podcast "%s"',
+                    'For user "%s" we found %d new and %d heard episodes for podcast "%s". Latest episode was "%s"',
                     $msg->body,
                     $new,
                     $heard,
-                    $podcast['name']
+                    $podcast['name'],
+                    $latest
                 )
             );
 
-            $this->logger->info(sprintf('Latest episode for podcast "%s" is "%s"', $podcast['name'], $latest));
-
-            $date = new \DateTime($latest);
             $this->client->getClient()->update(
                 sprintf('/users/%s/podcasts/%s', $msg->body, $i),
                 [
                     'newEpisodes' => $new,
-                    'latest' => $date->format(\DateTime::ISO8601)
+                    'latest' => $latest
                 ]
             );
         }
