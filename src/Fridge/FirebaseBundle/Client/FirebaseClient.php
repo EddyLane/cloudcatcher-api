806cf89b69c6089172c20bc9357b274221e72e4e
Eddy Lane <eddy@uvd.co.uk>
2014-08-29 14:55:45 +0100

    Heroku rabbitmq tuning

--- src/Fridge/FirebaseBundle/Client/FirebaseClient.php
+++ src/Fridge/FirebaseBundle/Client/FirebaseClient.php
@@ -10,13 +10,16 @@ namespace Fridge\FirebaseBundle\Client;
 
 use Firebase\Firebase;
 use GuzzleHttp\Client;
+use Monolog\Logger;
 
 class FirebaseClient
 {
     protected $client;
 
-    public function __construct($baseUrl, $secret)
+    public function __construct($baseUrl, $secret, Logger $logger)
     {
+        $logger->debug(sprintf('Connecting to firebase base_url "%s" secret "%s"', $baseUrl, $secret));
+
         $this->client = new Firebase([
             'base_url' => $baseUrl,
             'token' => $secret
--- src/Fridge/FirebaseBundle/Consumer/ReadNode.php
+++ src/Fridge/FirebaseBundle/Consumer/ReadNode.php
@@ -10,6 +10,7 @@ namespace Fridge\FirebaseBundle\Consumer;
 
 use Fridge\FirebaseBundle\Client\FirebaseClient;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
 use PhpAmqpLib\Message\AMQPMessage;
 use Symfony\Component\HttpKernel\Log\LoggerInterface;
@@ -20,6 +21,8 @@ class ReadNode implements ConsumerInterface
     private $xmlReader;
     private $client;
 
+    public static $googleFeedApi = 'https://ajax.googleapis.com/ajax/services/feed/load';
+
     public function __construct(FirebaseClient $client, LoggerInterface $logger)
     {
         $this->logger = $logger;
@@ -29,58 +32,83 @@ class ReadNode implements ConsumerInterface
 
     public function execute(AMQPMessage $msg)
     {
-        $data = $this->client->getClient()->get('/users/' . $msg->body . '/podcasts');
-        $this->logger->info(sprintf('Node processing:', $msg->body));
-        $guzzleClient = new Client();
+        try {
 
-        foreach ($data as $i => $podcast) {
+            $this->logger->info(sprintf('Node processing: %s', $msg->body));
 
-            $new = 0;
-            $heard = 0;
+            try {
+                $data = $this->client->getClient()->get('/users/' . $msg->body . '/podcasts');
+            }
+            catch (RequestException $e) {
+                $this->logger->error(sprintf('Curl failed to connect to firebase for user "%s". Request: "%s"', $msg->body, $e->getRequest()));
+                throw $e;
+            }
 
-            $responseJson = $guzzleClient->get(
-                'https://ajax.googleapis.com/ajax/services/feed/load',
-                [
+            $guzzleClient = new Client();
+
+            foreach ($data as $i => $podcast) {
+
+                $new = 0;
+                $heard = 0;
+
+                $options = [
                     'query' => [
                         'q' => $podcast['feed'],
                         'v' => '1.0',
                         'output' => 'json_xml',
                         'num' => -1
                     ]
-                ]
-            )->json()['responseData'];
+                ];
 
-            $xml = simplexml_load_string($responseJson['xmlString']);
-            foreach ($xml->xpath('//enclosure') as $episode) {
-                if (isset($podcast['heard']) && in_array($episode->attributes()->url, $podcast['heard'])) {
-                    $heard++;
-                } else {
-                    $new++;
+
+                try {
+                    $response = $guzzleClient->get(self::$googleFeedApi, $options);
+                    $responseJson = $response->json()['responseData'];
                 }
+                catch(RequestException $e) {
+                    $this->logger->error(sprintf('Curl failed to url "%s" with feed "%s". Request: "%s"', self::$googleFeedApi, $podcast['feed'], $e->getRequest()));
+                    throw $e;
+                }
+
+
+                $xml = simplexml_load_string($responseJson['xmlString']);
+
+                foreach ($xml->xpath('//enclosure') as $episode) {
+                    if (isset($podcast['heard']) && in_array($episode->attributes()->url, $podcast['heard'])) {
+                        $heard++;
+                    } else {
+                        $new++;
+                    }
+                }
+
+                $date = new \DateTime($responseJson['feed']['entries'][0]['publishedDate']);
+                $latest = $date->format(\DateTime::ISO8601);
+
+                $this->logger->info(
+                    sprintf(
+                        'For user "%s" we found %d new and %d heard episodes for podcast "%s". Latest episode was "%s"',
+                        $msg->body,
+                        $new,
+                        $heard,
+                        $podcast['name'],
+                        $latest
+                    )
+                );
+
+                $this->client->getClient()->update(
+                    sprintf('/users/%s/podcasts/%s', $msg->body, $i),
+                    [
+                        'newEpisodes' => $new,
+                        'latest' => $latest
+                    ]
+                );
             }
-
-            $date = new \DateTime($responseJson['feed']['entries'][0]['publishedDate']);
-            $latest = $date->format(\DateTime::ISO8601);
-
-            $this->logger->info(
-                sprintf(
-                    'For user "%s" we found %d new and %d heard episodes for podcast "%s". Latest episode was "%s"',
-                    $msg->body,
-                    $new,
-                    $heard,
-                    $podcast['name'],
-                    $latest
-                )
-            );
-
-            $this->client->getClient()->update(
-                sprintf('/users/%s/podcasts/%s', $msg->body, $i),
-                [
-                    'newEpisodes' => $new,
-                    'latest' => $latest
-                ]
-            );
         }
+        catch (\Exception $e) {
+            $this->logger->error(sprintf('Consumption failed: Exception "%s", Code: "%s", Message: "%s"', get_class($e), $e->getCode(), $e->getMessage()));
+            //return false;
+        }
+
 
     }
 
